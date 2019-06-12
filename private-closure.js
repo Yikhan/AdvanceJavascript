6e4e0aeb4cb0f162f3b1f6bf8df689e999ed1a08
Yihan <yihan.xiao@recreo.com.au>
2019-06-12 15:32:08 +1000

    closure practice

---
+++ private-closure.js
@@ -0,0 +1,57 @@
+
+
+var person = (function() {
+    var age = 25; //私有变量
+    return {
+        name: "Nicholas",
+        getAge: function () {
+            return age;
+        },
+        growOlder: function () {
+            age++;
+        }
+    }; 
+}());
+
+console.log(person.age);
+console.log(person.getAge())
+
+function find2(firstName, lastName)
+{　
+    var result = [];　　
+    for (var i = 0; i < this.names.length; i++)
+    {　　　　
+        if (this.names[i] === (firstName + " " + lastName))
+        {　　　　　　
+            result.push(this.names[i]);　　　　
+        }　　
+    }　　
+    return result;
+}
+
+function addMethod(f) {
+    if (f.length === 2) {
+        console.log("yes");
+    }
+    else {
+        console.log("no");
+        
+    }
+}
+
+var people = {　　
+    names: ["Dean Edwards", "Alex Russell", "Dean Tom"]
+};
+people["find here"] = function () {
+    console.log("find it");
+}
+var old = people["find here"]
+
+function test (a,b,c) {
+    return a+b+c
+}
+console.log();
+
+
+
+
